a06f7689ebc2f758884fbb5ba13cebd00fc02d81
User07 <user07@user07de-imac.local>
2018-09-19 14:20:16 +0800

    Initial Commit

---
+++ workTests/workTests.m
@@ -0,0 +1,37 @@
+//
+//  workTests.m
+//  workTests
+//
+//  Created by User07 on 2018/9/19.
+//  Copyright © 2018年 321. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface workTests : XCTestCase
+
+@end
+
+@implementation workTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
